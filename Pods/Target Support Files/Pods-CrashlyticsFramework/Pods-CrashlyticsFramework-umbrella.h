d1711aeb9b1aee8cd074b374d54f778a8e8634fe
Maxim Zabelin <Maxim.Zabelin@noveogroup.com>
2015-05-21 19:45:03 +0600

    This commit is made in order to demonstrate the CocoaPods#3582-issue.

--- Pods/Target Support Files/Pods-CrashlyticsFramework/Pods-CrashlyticsFramework-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "Crashlytics.h"
-
-FOUNDATION_EXPORT double CrashlyticsFrameworkVersionNumber;
-FOUNDATION_EXPORT const unsigned char CrashlyticsFrameworkVersionString[];
-
